e3038dc2afcfddc8282b78c0336f96dee8a4e2cf
Rhino Lu <rhinolu@gmail.com>
2015-10-12 12:28:40 +0800

    fix to current class model
    Please check again. If there still have any mistake, just tell me.
    Thanks.

--- Kaleidoscope/Kaleidoscope.js
+++ Kaleidoscope/Kaleidoscope.js
@@ -35,87 +35,40 @@
  * @class Kaleidoscope
  * @param radius {Number} The radius of the kaleidoscope.
  * @param source {DisplayObject} The DisplayObject to use as the source.
+ * The DisplayObject that will be drawn into each slice. Any transformations on the
+ * source display object will be applied when the slice is drawn.
+ * The source will be drawn multiple times (ex. 20 times for a 10 slice kaleidoscope).
+ * If the source is expensive to draw (a complex Shape, or Container with
+ * many children), then you may wish to pre-render the source a single time using
+ * the cache() method.
  * @param [slices=8] {Number} The number of slices to draw.
  * @param [pattern] {Array} Pattern of relative ratios for the slice sizes. For example, passing [1,2] would result in every second slice being twice as large as it's neighbors.
+ * @param [pad=0] {Number}
+ * Specifies the amount to overlap each slice with its neighbours to reduce seams
+ * between them. Generally a value of 0.6 works well, but you may wish to adjust this
+ * for sources with transparency.
+ * You may still see seams if your source draws multiple times into each slice. For
+ * example, a Container with multiple children. Pre-rendering the source by using the
+ * cache() method should resolve this.
  * @extends DisplayObject
  * @constructor
  **/
-function Kaleidoscope (radius, source, slices, pattern) {
-  this.initialize(radius, source, slices, pattern);
+function Kaleidoscope (radius, source, slices, pattern, pad) {
+  this.DisplayObject_constructor();
+  if (!pattern) { pattern = [1]; }
+  if (!slices) { slices = 8; }
+  if (!pad) { pad = 0; }
+  this.radius = radius;
+  this.source = source;
+  this.pad = pad;
+  var l = pattern.length;
+  if (slices/pattern.length%1 != 0) { throw "slices must be divisible by pattern length"; }
+  var ttl = 0;
+  for (var i=0; i<slices; i++) { ttl += pattern[i%l]; }
+  var s = this._slices = [];
+  for (var i=0; i<slices; i++) { s[i] = pattern[i%l]/ttl*Math.PI; }
 }
 var p = createjs.extend(Kaleidoscope, createjs.DisplayObject);
-
-// public properties:
-	/**
-	 * The DisplayObject that will be drawn into each slice. Any transformations on the
-	 * source display object will be applied when the slice is drawn.
-	 *
-	 * The source will be drawn multiple times (ex. 20 times for a 10 slice kaleidoscope).
-	 * If the source is expensive to draw (a complex Shape, or Container with
-	 * many children), then you may wish to pre-render the source a single time using
-	 * the cache() method.
-	 * @property source
-	 * @type DisplayObject
-	 * @default null
-	 **/
-	p.source = null;
-
-	/**
-	 * The radius of the kaleidoscope in pixels.
-	 * @property radius
-	 * @type Number
-	 * @default 0
-	 **/
-	p.radius = 0;
-
-	/**
-	 * Specifies the amount to overlap each slice with its neighbours to reduce seams
-	 * between them. Generally a value of 0.6 works well, but you may wish to adjust this
-	 * for sources with transparency.
-	 *
-	 * You may still see seams if your source draws multiple times into each slice. For
-	 * example, a Container with multiple children. Pre-rendering the source by using the
-	 * cache() method should resolve this.
-	 * @property pad
-	 * @type Number
-	 * @default 0
-	 **/
-	p.pad = 0;
-
-// private properties:
-	/**
-	 * @property _slices
-	 * @type Array
-	 * @private
-	 **/
-	p._slices = null; // size in radians of each slice.
-
-// constructor:
-	/**
-	 * @property DisplayObject_constructor
-	 * @type Function
-	 * @private
-	 **/
-	p.DisplayObject_constructor = p.initialize;
-
-	/**
-	 * Initialization method.
-	 * @method initialize
-	 * @protected
-	*/
-	p.initialize = function(radius, source, slices, pattern) {
-		this.DisplayObject_constructor();
-		if (!pattern) { pattern = [1]; }
-		if (!slices) { slices = 8; }
-		this.radius = radius;
-		this.source = source;
-		var l = pattern.length;
-		if (slices/pattern.length%1 != 0) { throw "slices must be divisible by pattern length"; }
-		var ttl = 0;
-		for (var i=0; i<slices; i++) { ttl += pattern[i%l]; }
-		var s = this._slices = [];
-		for (var i=0; i<slices; i++) { s[i] = pattern[i%l]/ttl*Math.PI; }
-	}
 
 // public methods:
 	/**
@@ -129,13 +82,6 @@ var p = createjs.extend(Kaleidoscope, createjs.DisplayObject);
 		var hasContent = this.cacheCanvas || (this._slices && this.source);
     return !!(this.visible && this.alpha > 0 && this.scaleX != 0 && this.scaleY != 0 && hasContent);
 	}
-
-	/**
-	 * @property DisplayObject_draw
-	 * @type Function
-	 * @private
-	 **/
-	p.DisplayObject_draw = p.draw;
 
 	/**
 	 * Draws the display object into the specified context ignoring it's visible, alpha, shadow, and transform.
@@ -170,7 +116,6 @@ var p = createjs.extend(Kaleidoscope, createjs.DisplayObject);
 	p.toString = function() {
 		return "[Kaleidoscope]";
 	}
-
 
 // private methods:
 	p._drawSlice = function(ctx, r, a, mirror, pad) {
